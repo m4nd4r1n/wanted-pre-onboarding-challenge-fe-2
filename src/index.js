228582530929247f837eb5048e7685c0c13d483c
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-02 20:50:50 +0900

    feat: Todo type

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+"use strict";
+
+/**
+ * Todo 타입입니다.
+ *
+ * @typedef {Object} Todo
+ * @property {number} id 고유 아이디
+ * @property {boolean} done 완료여부
+ * @property {string} content 내용
+ * @property {string} category 카테고리
+ * @property {string[]} [tags] 태그들
+ */
 
